48308020b8fce17e03629de716a804e2604f4df1
Your Name ThangDoanTMA <dhthang@tma.com.vn>
2022-03-23 17:51:26 +0700

    Upload file to build project employee manager

---
+++ src/pages/home/EmployeeManagerComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function EmployeeManagerComponent() {
+  return (
+    <div className='container'>
+      <h3>Employee Manager</h3>
+    </div>
+  );
+}
---
+++ src/pages/team/TeamComponent.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TeamComponent() {
+  return <div>TeamComponent</div>;
+}
